afc2217abb96f4088a650a17f46e71502a4a615a
DanielCarpov <dcarpov@endava.com>
2019-10-29 11:57:49 +0200

    Add project files.

---
+++ PlanificatorCMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlanificatorCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
