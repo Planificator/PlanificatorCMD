09e0ae1dcb8df5e38fad62aa0d5499a87ab03b31
Cristi Plamadeala <chris.plamadeala@gmail.com>
2019-10-30 14:33:03 +0200

    Deleted Comments

--- PlanificatorCMD/Program.cs
+++ PlanificatorCMD/Program.cs
@@ -8,19 +8,7 @@ namespace PlanificatorCMD
     {
         static void Main(string[] args)
         {
-            //appsettings.json configuration
-            //var builder = new ConfigurationBuilder()
-            //    .SetBasePath(Directory.GetCurrentDirectory())
-            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-            //IConfigurationRoot configuration = builder.Build();
-
-            //Console.WriteLine(configuration.GetConnectionString("Default"));
-
-        //    public static IHostBuilder CreateHostBuilder(string[] args)
-        //=> Host.CreateDefaultBuilder(args)
-        //    .ConfigureWebHostDefaults(
-        //        webBuilder => webBuilder.UseStartup<Startup>());
 
         }
     }
