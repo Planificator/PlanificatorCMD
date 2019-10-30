7514a2af7742acbabc1e8ea318048210501b8b1a
Cristi Plamadeala <chris.plamadeala@gmail.com>
2019-10-30 15:36:47 +0200

    Added Company to SpeakerProfile; -Added New Migration

---
+++ PlanificatorCMD/Migrations/20191030133404_Updated-SpeakerProfile.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlanificatorCMD.Migrations
+{
+    public partial class UpdatedSpeakerProfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_SpeakerProfiles_PhotoID",
+                table: "Photos");
+
+            migrationBuilder.RenameColumn(
+                name: "SpeakerID",
+                table: "SpeakerProfiles",
+                newName: "SpeakerId");
+
+            migrationBuilder.RenameColumn(
+                name: "PhotoID",
+                table: "Photos",
+                newName: "PhotoId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Company",
+                table: "SpeakerProfiles",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_SpeakerProfiles_PhotoId",
+                table: "Photos",
+                column: "PhotoId",
+                principalTable: "SpeakerProfiles",
+                principalColumn: "SpeakerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_SpeakerProfiles_PhotoId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "Company",
+                table: "SpeakerProfiles");
+
+            migrationBuilder.RenameColumn(
+                name: "SpeakerId",
+                table: "SpeakerProfiles",
+                newName: "SpeakerID");
+
+            migrationBuilder.RenameColumn(
+                name: "PhotoId",
+                table: "Photos",
+                newName: "PhotoID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_SpeakerProfiles_PhotoID",
+                table: "Photos",
+                column: "PhotoID",
+                principalTable: "SpeakerProfiles",
+                principalColumn: "SpeakerID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
