32f03b9cd467a09a2dd0f78d47a4b33f563acb58
Vasily <vasilypascal@gmail.com>
2019-10-31 12:38:13 +0200

    created class ShowSpeakersVerb + interface IShowSpeakersVerb

---
+++ PlanificatorCMD/Verbs/IShowSpeakersVerb.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanificatorCMD.Verbs
+{
+    public interface IShowSpeakersVerb
+    {
+    }
+}
---
+++ PlanificatorCMD/Verbs/ShowSpeakersVerb.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanificatorCMD.Verbs
+{
+    [Verb("show_speakers", HelpText = "Showing all speakers")]
+    public class ShowSpeakersVerb : IShowSpeakersVerb
+    {
+        [Option('o',"option", HelpText = "Display Option: \"True\" - Showing all Speakers. \"False\" - Showing first 10 Speakers.", Default = false)]
+        public bool DisplayOption { get; set; }
+    }
+}
