08ed8dc03b5473452cd8d19edb2845ca54c7aff4
Vasily <vasilypascal@gmail.com>
2019-11-01 12:41:59 +0200

    Changed List to IEnumerable parameter in DisplaySpeakers. Added new test DisplaySpeakersTests + 2 tests for method DisplayAllSpeakers

---
+++ PlanificatorCMD.Tests/DisplaySpeakersTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using PlanificatorCMD.Core;
+using PlanificatorCMD.Utils;
+
+namespace PlanificatorCMD.Tests
+{
+    public class DisplaySpeakersTests
+    {
+        [Fact]
+        public void DisplayAllSpeakers_ReturnFalse_WithEmptySpeakersList()
+        {
+            List<SpeakerProfile> speakers = null;
+            bool displayOption = true;
+
+            var expected = false;
+            DisplaySpeakers sut = new DisplaySpeakers();
+            var actual = sut.DisplayAllSpeakers(speakers, displayOption);
+
+            Assert.Equal(actual, expected);
+
+        }
+
+        [Fact]
+        public void DisplayAllSpeakers_ReturnTrue_WithValidSpeakersList()
+        {
+            List<SpeakerProfile> speakers = new List<SpeakerProfile>() {
+                new SpeakerProfile()
+                {
+                FirstName = "Vasily",
+                LastName = "Pascal",
+                Bio = "I'm .NET intern",
+                Email = "vasilypascal@gmail.com",
+                Company = "Endava",
+                Photo = new Photo
+                {
+                    Path = @"...\something\13.jpg"
+                }}, new SpeakerProfile(){ FirstName = "Valentin",
+                LastName = "Butnaru",
+                Bio = "I'm .NET intern",
+                Email = "valentin@gmail.com",
+                Company = "Endava",
+                Photo = new Photo
+                {
+                    Path = @"...\something\14.jpg"
+                }} };
+
+            bool displayOption = true;
+
+            var expected = true;
+            DisplaySpeakers sut = new DisplaySpeakers();
+            var actual = sut.DisplayAllSpeakers(speakers, displayOption);
+
+            Assert.Equal(actual, expected);
+
+        }
+
+
+    }
+}
