0a0588cb80bdc2615cf6b981a9ea636f2f4d84c0
Vasily <vasilypascal@gmail.com>
2019-11-01 17:14:10 +0200

    Added implementation to method MapToPresentationTags of class PresentationMapper

--- PlanificatorCMD/Utils/PresentationMapper.cs
+++ PlanificatorCMD/Utils/PresentationMapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PlanificatorCMD.Core;
+using PlanificatorCMD.Verbs;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,29 @@ namespace PlanificatorCMD.Utils
 {
     class PresentationMapper
     {
+        public ICollection<PresentationTag> MapToPresentationTag (Add_PresentationVerb addPresentationVerb)
+        {
+            ICollection<string> tags = addPresentationVerb.Tags.Split(" ");
+
+            Presentation presentation = new Presentation()
+            {
+                Title = addPresentationVerb.Title,
+                ShortDescription = addPresentationVerb.ShortDescription,
+                LongDescription = addPresentationVerb.LongDescription
+            };
+
+            List<PresentationTag> presentationTags = new List<PresentationTag>();
+
+            foreach (var tagName in tags) 
+            {
+                PresentationTag presentationTag = new PresentationTag()
+                {
+                    Presentation = presentation,
+                    Tag = new Tag() { TagName = tagName }
+                };
+                presentationTags.Add(presentationTag);
+            }
+            return presentationTags;
+        }
     }
 }
