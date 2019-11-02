e17c04b1ca0433699f8d88c87bc02f3cd17bc7d9
Daniel Carpov <daniel.carpov@endava.com>
2019-11-02 13:58:14 +0200

    AddPresentationVerbValidator - done

---
+++ PlanificatorCMD/Validators/IAddPresentationVerbValidator.cs
@@ -0,0 +1,9 @@
+﻿using PlanificatorCMD.Verbs;
+
+namespace PlanificatorCMD.Validators
+{
+    interface IAddPresentationVerbValidator
+    {
+        bool IsValid(IAddPresentationVerb addPresentationVerb);
+    }
+}
