849b877c2630b1fae17802e71331e870a2807d5d
Cristi Plamadeala <chris.plamadeala@gmail.com>
2019-11-08 17:01:44 +0200

    Covered PresentationRepository with Tests

--- PlanificatorCMD.Tests/PresentationRepositoryTests/PresentationRepositoryTests.cs
+++ PlanificatorCMD.Tests/PresentationRepositoryTests/PresentationRepositoryTests.cs
@@ -189,5 +189,101 @@ namespace PlanificatorCMD.Tests.PresentationRepositoryTests
             }
         }
 
+        [Fact]
+        public void GetPresentationsCount_returns_presentations_count()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<PlanificatorDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new PlanificatorDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+
+                    var service = new PresentationRepository(context);
+
+                    var testData1 = new PresentationRepositoryTestsData();
+                    var testData2 = new PresentationRepositoryTestsData();
+                    var testData3 = new PresentationRepositoryTestsData();
+
+                    List<Presentation> presentations = new List<Presentation>();
+                    presentations.Add(testData1.presentation);
+                    presentations.Add(testData2.presentation);
+                    presentations.Add(testData3.presentation);
+
+
+                    service.AddPresentation(testData1.presentationTags);
+                    service.AddPresentation(testData2.presentationTags);
+                    service.AddPresentation(testData3.presentationTags);
+
+                    context.SaveChanges();
+
+                    Assert.Equal(presentations.Count(), service.GetPresentationCount());
+
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Fact]
+        public void AssignSpeakerToPresentation_writes_to_database()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            SpeakerProfile speaker = new SpeakerProfile
+            {
+                FirstName = "Test First Name",
+                LastName = "Test Last Name",
+                Email = "test@email.net",
+                Bio = "My test bio",
+                Company = "Test company",
+                Photo = new Photo { Path = "Test Path" }
+            };
+
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<PlanificatorDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new PlanificatorDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+
+                    var presentationService = new PresentationRepository(context);
+                    var speakerServie = new SpeakerRepository(context);
+
+                    var testData = new PresentationRepositoryTestsData();
+
+                    speakerServie.AddSpeakerProfile(speaker);
+                    presentationService.AddPresentation(testData.presentationTags);
+                    presentationService.AssignSpeakerToPresentation(speaker, 0);
+
+                    context.SaveChanges();
+
+                    Assert.Equal(1, context.SpeakerProfiles.Count());
+                    Assert.Equal(1, context.Presentations.Count());
+                    Assert.Equal(1, context.PresentationSpeakers.Count());
+                    Assert.Equal(speaker, context.PresentationSpeakers.Include(p => p.SpeakerProfile).Single().SpeakerProfile);
+                    Assert.Equal(testData.presentation, context.PresentationSpeakers.Include(p => p.Presentation).Single().Presentation);
+
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
     }
 }
